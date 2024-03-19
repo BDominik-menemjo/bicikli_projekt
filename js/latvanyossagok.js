d5d98412ba2aea48440c1a98683b99f97f549229
Kovács Kristóf <77266066+K2005cpu@users.noreply.github.com>
2024-03-19 08:01:01 +0100

    Update latvanyossagok.js

--- js/latvanyossagok.js
+++ js/latvanyossagok.js
@@ -3,5 +3,5 @@ function Mellszobor(){
     tartalom.innerHTML="<p>Csokonai Vitéz Mihály hires magyar költő volt a 18. század idején. Dunaalmáson különlegesen nagy kultúrája van, hiszen szeremle, Vajda Julianna, múzsai nevén Lilla itt élt. Eme mellszobrot Szabados Béla szobrászművész készitette 1996-ban.</p><img src=\"../media/images/mellszobor.jpg\"style=\"width:100%\">"
 }
 function To(){
-    tartalom.innerHTML="<p>A Dunaalmási Pihenő-tó eredetileg strandként üzemelt, viszont a bezárása után a település önkorményzata elhanyagolta. Azonban Ollé Árpád polgermester tervei keretein belül a falu lakosainak örömére felújitották.</p><img src=\"../media/images/a8.jpg\" >"
+    tartalom.innerHTML="<p>A Dunaalmási Pihenő-tó eredetileg strandként üzemelt, viszont a bezárása után a település önkorményzata elhanyagolta. Azonban Ollé Árpád polgermester tervei keretein belül a falu lakosainak örömére felújitották.</p><img src=\"../media/images/a8.jpg\" style=\"width:100%\">"
 }
